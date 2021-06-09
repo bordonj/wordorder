394bff58b846fc1b73322e1820dfd543bfb57975
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-06-09 06:48:12 -0700

    add code to combine word and counter in array

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,50 @@
+let topList;
 let count = (sentence) => {
   let senArr = sentence.trim().split(' ');
-  let senArrCopy = senArr.slice();
   let reorderedArr = [];
   let existingWord = [];
-  let pair = [];
-  let counter = 0;
-  for (let i = 0; i < senArr.length; i++) {
-    // if (existingWord.includes(senArr[i])) {
-    //   return;
-    // }
-    senArrCopy.forEach(function(element) {
-      if (senArr[i] === element) {
-        if (!existingWord.includes(senArr[i])) {
-          existingWord.push(senArr[i]);
-          counter = 1;
-        } else if (existingWord.includes(senArr[i])) {
-          counter++;
-        }
-      } else if (senArr[i] !== element) {
-        if (!existingWord.includes(element)) {
-          existingWord.push(element);
-        }
-        counter = 1;
-      }
-      senArrCopy.shift();
-      // console.log('senArr[i]', senArr[i], 'counter', counter);
-      let varPair = `${senArr[i]} - ${counter}`;
-      console.log('varPair', varPair);
-      if (pair.includes(varPair)) {
-        return varPair;
-      } else {
-        pair.push(varPair);
-        console.log('pair', pair);
-      }
-    })
-    // pair.push(`${senArr[i]} - ${counter}`);
-    // console.log('pair', pair);
-  }
-  // console.log('existing word', existingWord);
-}
+  let pairs = [];
+  let wordCount = [];
 
+  for (let i = 0; i < senArr.length; i++) {
+    if (!existingWord.includes(senArr[i])) {
+      existingWord.push(senArr[i]);
+    }
+  }
+  for (let word of existingWord) {
+    let counter = 0;
+    for (let element of senArr) {
+      if(word === element) {
+        counter++;
+      }
+    } 
+    pairs.push([word, counter]);
+    wordCount.push(counter);
+  }
+  pairs.sort((a, b) => b[1] - a[1]);
+  console.log('pairs', pairs);
+  console.log('existing word', existingWord);
+  console.log('wordcount', wordCount);
+  console.log('pairs.length', pairs.length);
+
+  let topThree = 'none';
+  let topOne = `${pairs[0][0]} - ${pairs[0][1]}x`;
+  let topTwo = `${pairs[1][0]} - ${pairs[1][1]}x`;
+  if (pairs.length > 2) {
+    topThree = `${pairs[2][0]} - ${pairs[2][1]}x`;
+  }
+
+
+  return topList = [topOne, topTwo, topThree];
+}
+console.log(topList);
 $(document).ready(function() {
   $('form#userInput').submit(function(e) {
     e.preventDefault();
     let sentence = $('input#userSentence').val();
     count(sentence);
+    $('.topWords').append(`<li>${topList[0]}</li>`)
+    $('.topWords').append(`<li>${topList[1]}</li>`)
+    $('.topWords').append(`<li>${topList[2]}</li>`)
   })
 })
